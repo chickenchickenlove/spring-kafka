5ee454e670cb0a60e8ce8a20ae87b6ea28ffc798
ChickenchickenLove <ojt90902@naver.com>
2024-03-24 09:18:45 +0900

    Apply CodeReview

--- spring-kafka/src/main/java/org/springframework/kafka/config/ParallelConsumerConfig.java
+++ spring-kafka/src/main/java/org/springframework/kafka/config/ParallelConsumerConfig.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.kafka.config;
 
 import java.util.Objects;
@@ -6,6 +22,13 @@ import org.apache.kafka.clients.consumer.Consumer;
 
 import io.confluent.parallelconsumer.ParallelConsumerOptions;
 import io.confluent.parallelconsumer.ParallelConsumerOptions.ProcessingOrder;
+
+/**
+ * ParallelConsumerConfig is for config of io.confluent.parallelconsumer.
+ * This will be registered as Spring Bean when {@link ParallelConsumerConfiguration}'s condition function is true.
+ * @author Sanghyeok An
+ * @since 3.2.0
+ */
 
 public class ParallelConsumerConfig {
 
--- spring-kafka/src/main/java/org/springframework/kafka/config/ParallelConsumerConfiguration.java
+++ spring-kafka/src/main/java/org/springframework/kafka/config/ParallelConsumerConfiguration.java
@@ -1,6 +1,21 @@
+/*
+ * Copyright 2014-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.kafka.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
@@ -11,37 +26,36 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.ParallelConsumerCallback;
 import org.springframework.kafka.core.ParallelConsumerFactory;
 
-import java.util.Objects;
+/**
+ * If User decide to use parallelConsumer on SpringKafka, User should import this class to their ComponentScan scopes.
+ * If so, this class will register both {@link ParallelConsumerContext} and {@link ParallelConsumerFactory} as Spring Bean.
+ * User has responsibility to include this file to their componentScan scopes, to register ConcreteClass of {@link ParallelConsumerCallback}.
+ * @author Sanghyeok An
+ * @since 3.2.0
+ */
 
 @Configuration
 public class ParallelConsumerConfiguration<K, V> {
-
-	@Autowired(required = false)
-	ParallelConsumerCallback<K, V> parallelConsumerCallback;
 
 
 	static class EnableParallelConsumerCondition implements Condition {
 		@Override
 		public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-			String isEnable = context.getEnvironment().getProperty(ParallelConsumerConfig.PARALLEL_CONSUMER_ENABLE);
-			if (Objects.isNull(isEnable)) {
-				return false;
-			}
-			return "true".equals(isEnable.toLowerCase());
+			return Boolean.valueOf(context.getEnvironment().getProperty(ParallelConsumerConfig.PARALLEL_CONSUMER_ENABLE));
 		}
 	}
 
 	@Bean(name = ParallelConsumerContext.BEAN_NAME)
 	@Conditional(EnableParallelConsumerCondition.class)
-	public ParallelConsumerContext<K,V> parallelConsumerContext() {
-		return new ParallelConsumerContext(this.parallelConsumerCallback);
+	public ParallelConsumerContext<K,V> parallelConsumerContext(ParallelConsumerCallback<K, V> parallelConsumerCallback) {
+		return new ParallelConsumerContext(parallelConsumerCallback);
 	}
 
 	@Bean(name = ParallelConsumerFactory.BEAN_NAME)
 	@Conditional(EnableParallelConsumerCondition.class)
-	public ParallelConsumerFactory<K,V> parallelConsumerFactory(DefaultKafkaConsumerFactory consumerFactory) {
-		return new ParallelConsumerFactory(parallelConsumerContext(),
+	public ParallelConsumerFactory<K,V> parallelConsumerFactory(DefaultKafkaConsumerFactory consumerFactory,
+																ParallelConsumerCallback<K, V> parallelConsumerCallback) {
+		return new ParallelConsumerFactory(parallelConsumerContext(parallelConsumerCallback),
 				consumerFactory);
 	}
-
 }
